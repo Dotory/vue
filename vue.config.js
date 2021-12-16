359cfa086ec037e79cee37bd02361cda230f70db
jungmin <jungmin.lee@emotion.co.kr>
2021-12-16 17:21:03 +0900

    tet

---
+++ vue.config.js
@@ -0,0 +1,23 @@
+
+module.exports = {
+  /*css: {
+    loaderOptions: {
+      scss: {
+        prependData: `
+            @import "@/assets/scss/_variables.scss";
+            @import "@/assets/scss/_mixins.scss";
+            @import "@/assets/scss/_extend.scss";
+        `,
+      },
+    },
+  },*/
+  outputDir: './docs',
+  publicPath: '/vue/',
+  configureWebpack: {
+    //entry: ['babel-polyfill', './src/main.js'],
+  },
+  devServer: {
+    port: 8080,
+    overlay: false,
+  },
+};
